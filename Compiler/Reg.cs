32af13b29ab6e271a9e6a5931971e686baeebee4
Bobrek <Bobrek@DESKTOP-EJL433M>
2021-11-22 22:08:38 +0700

    Добавьте файлы проекта.

---
+++ Compiler/Reg.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    public class Reg
+    {
+        int type;
+        private char state;
+        public Reg(int t)
+        {
+            type = t;
+        }
+        public bool CheckString(string input)
+        {
+            switch (type)
+            {
+                case 1:
+                    {
+                        Regex reg = new Regex(@"0*10*10*10*");
+                        MatchCollection matches = reg.Matches(input);
+                        return matches.Count != 0;
+                        break;
+                    }
+                case 2:
+                    {
+                        Regex reg = new Regex(@"[A-Z][a-z]* [A-Z]\.[A-Z]\.");
+                        MatchCollection matches = reg.Matches(input);
+                        return matches.Count != 0;
+                        break;
+                    }
+                case 3:
+                    {
+                        state = '1';
+                        if (input.Length == 0)
+                            return false;
+                        foreach (var item in input)
+                        {
+                     
+                        switch(state)
+                            {
+                                case '1': { if (item == 'a') state = '2'; break; }
+                                case '2': { if (item == 'a') state = '2'; else if (item == 'b') state = '3'; break; }
+                                case '3': { if (item == 'c') state = '4'; break; }
+                                case '4': { if (item == 'c') state = '5'; break; }
+                                default: return false;
+                            }
+                        }
+                        if (state == '5')
+                            return true;
+                        else
+                            return false;
+                        break;
+                    }
+                case 4:
+                    {
+                        Regex reg = new Regex(@"10*(123)+");
+                        MatchCollection matches = reg.Matches(input);
+                        return matches.Count != 0;
+                        break;
+                    }
+                default:
+                    break;
+            }
+            return false;
+        }
+    }
+}
---
+++ Compiler/SyntaxErrListener.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+using System.IO;
+
+namespace Compiler
+{
+    public class SyntaxError
+
+    {
+        public TextWriter output;
+        public IRecognizer recognizer;
+        public IToken offendingSymbol;
+        public int line;
+        public int charPositionInLine;
+        public string msg;
+        public RecognitionException e;
+        public SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+
+        {
+            this.output = output;
+            this.recognizer = recognizer;
+            this.offendingSymbol = offendingSymbol;
+            this.line = line;
+            this.charPositionInLine = charPositionInLine;
+            this.e = e;
+            this.msg = msg;
+        }
+
+        public override string ToString()
+
+        {
+            string str = "Errors: ";
+            str += msg;
+            str += ", in line " + line;
+            str += ":" + charPositionInLine;
+            return str;
+        }
+
+        public class SyntaxErrListener : BaseErrorListener
+
+        {
+            List<SyntaxError> syntaxErrors;
+            public List<SyntaxError> SyntaxErrors { get => syntaxErrors; }
+            public SyntaxErrListener()
+
+            {
+                syntaxErrors = new List<SyntaxError>();
+            }
+
+            public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+
+            {
+                syntaxErrors.Add(new SyntaxError(output, recognizer, offendingSymbol, line, charPositionInLine, msg, e));
+            }
+        }
+
+        public class SyntaxErrListenerLexer : IAntlrErrorListener<int>
+
+        {
+            List<SyntaxError> syntaxErrors;
+            public List<SyntaxError> SyntaxErrors { get => syntaxErrors; }
+            public SyntaxErrListenerLexer()
+            {
+                syntaxErrors = new List<SyntaxError>();
+            }
+
+            public void SyntaxError(TextWriter output, IRecognizer recognizer, int offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+
+            {
+                syntaxErrors.Add(new SyntaxError(output, recognizer, null, line, charPositionInLine, msg, e));
+            }
+
+        }
+
+    }
+
+}
